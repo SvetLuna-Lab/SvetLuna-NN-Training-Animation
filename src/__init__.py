d3d195e763123ff58efcfa73ac3e6c01a1fe5ba7
Svetlana Romanova <svetlana.ivan.romanova@gmail.com>
2025-11-05 09:37:42 +0300

    chore: add src package initializer

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+SvetLuna NN Training Animation package.
+
+This module exposes a simple Matplotlib-based animation
+that illustrates neural network training (loss & accuracy curves,
+plus a schematic network diagram).
+"""
